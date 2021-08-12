8cc02120531b1aaafb6a1480e6f0d9e5d08508ef
PlasticCafe <jdiaz0612@gmail.com>
2021-08-11 21:42:36 -0500

    Problem 8: strip earlier

--- python_leetcode/solutions/s0008.py
+++ python_leetcode/solutions/s0008.py
@@ -1,10 +1,10 @@
-
 import re
 class Solution:
     maxint_32 = 2**31 - 1
     minint_32 = -1*(2**31)
 
     def myAtoi(self, s:str) -> int:
+        s = s.lstrip(' ')
         if(len(s) == 0):
             return 0
         parseHead = 0
@@ -12,15 +12,13 @@ class Solution:
         result = 0
         numMatcher = re.compile('\d')
         signMatcher = re.compile('[\+\-]')
-        s = s.lstrip(' ')
         if(signMatcher.match(s[parseHead])):
             if s[parseHead] == '-' : negative = True
             parseHead += 1
-        while numMatcher.match(s[parseHead]):
+        while parseHead < len(s) and numMatcher.match(s[parseHead]):
             result *= 10
             result += ord(s[parseHead]) - 48
             parseHead += 1
-            if parseHead >= len(s): break
         if(negative): result *= -1
         if(result < self.minint_32): result = self.minint_32 
         elif(result > self.maxint_32): result = self.maxint_32
